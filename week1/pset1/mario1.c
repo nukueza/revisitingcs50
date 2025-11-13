d841ccbfc805cfb6c53ed0c63a9fde29880b5c8c
Nukueza <nukueza@gmail.com>
2025-11-13 17:42:54 +0700

    refactoring mario less comfortible

--- week1/pset1/mario1.c
+++ week1/pset1/mario1.c
@@ -1,21 +1,40 @@
 #include <stdio.h>
 
+// function prototipe: tells the compiler print_row exist and takes two ints
+void print_row(int spaces, int bricks);
+
 int main (void)
 {
-  int height = 5;
+  int height; 
+  do {
+    printf("height: ");
+    if (scanf("%d", &height) != 1)
+    {
+      // handle potential invalid input
+      printf("invalid input.\n");
+      return 1;
+    }
+  }while (height < 1);
+
+  // print a pyramid of that height
   for (int i = 0; i < height; i++)
   {
-    // print spaces for alignment
-    for (int k = 0; k < height - i - 1; k++)
-    {
-      printf(" ");
-    }
-    
-    // print '#' character
-    for (int j = 0; j <= i; j++)
-    {
-      printf("#");
-    }
-    printf("\n");
+    // calculate spaces and bricks for the current row i 
+    int spaces = height - i - 1;
+    int bricks = i + 1;
+    print_row(spaces, bricks); 
   }
+}
+
+void print_row(int spaces, int bricks)
+{
+  for (int j = 0; j < spaces; j++)
+  {
+    printf(" ");
+  }
+  for (int i = 0; i < bricks; i++)
+  {
+    printf("#"); 
+  }
+  printf("\n");
 }
